398c30d55c23dc8ca35350a3f30a9d741090683d
F3licia <romeofelicia@gmail.com>
2021-06-19 17:31:59 +0200

    inizio header

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,15 +1,29 @@
+@yield('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
-@yield('content')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('page_title')</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}"> <!--asset = public-->
 </head>
 <body>
+    <div class="topperbar">
+         <div class="mini-menu"><p>testo qui</p></div>
+    </div>
+    <div class="header">
+        <img src="{{asset('/images/dc-logo.png')}}" alt="">
+            <ul>
+                 <li>testo</li>
+                 <li>testo</li>
+                 <li>testo</li>
+                 <li>testo</li>
+                 <li>testo</li>
+            </ul>
+    </div>
+    <div class="hero"></div>
+    
 
     
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,4 @@
 @extends('layout.default')
 
-
 @section('page_title', 'pag_1')
 
-@section('content')
-<h1>Welcome</h1>
-@endsection
