1eb11b445c3db207a577e496c5f5346c9953a320
F3licia <romeofelicia@gmail.com>
2021-06-19 20:20:01 +0200

    creato array dati

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -15,6 +15,7 @@
     <div class="header">
     <img src="{{asset('/images/dc-logo.png')}}" alt="">
 
+
             <ul>           
                 <li>
                     <a href="{{ route('characters') }}">
@@ -25,6 +26,27 @@
                     </a>
                     <a href="{{ route('movies') }}">
                         movies
+                    </a>
+                    <a href="{{ route('tv') }}">
+                       tv
+                    </a>
+                    <a href="{{ route('games') }}">
+                       games
+                    </a>
+                    <a href="{{ route('collectibles') }}">
+                        collectibles
+                    </a>
+                    <a href="{{ route('video') }}">
+                        video
+                     </a>
+                     <a href="{{ route('fans') }}">
+                        fans
+                     </a>
+                     <a href="{{ route('news') }}">
+                         news
+                     </a>
+                     <a href="{{ route('shop') }}">
+                        shop
                     </a>
                 </li>
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,47 @@ Route::get('/characters', function () {
     return view('characters');
 })->name('characters');
 
-Route::get('comics', function () {
+Route::get('/comics', function () {
     return "";
 })->name('comics');
 
-Route::get('movies', function () {
+Route::get('/movies', function () {
     return "";
 })->name('movies');
+
+Route::get('/tv', function () {
+    return "";
+})->name('tv');
+
+Route::get('/games', function () {
+    return "";
+})->name('games');
+
+Route::get('/collectibles', function () {
+    return "";
+})->name('collectibles');
+
+Route::get('/video', function () {
+    return "";
+})->name('video');
+
+Route::get('/fans', function () {
+    return "";
+})->name('fans');
+
+Route::get('/news', function () {
+    return "";
+})->name('news');
+
+Route::get('/shop', function () {
+    return "";
+})->name('shop');
+
+Route::get('/currentSeries', function () {
+   $comics = config("comics");
+
+   $datiView = [
+       "comicsList" => $comics
+   ];
+    return view("currentSeries", $datiView);
+})->name('serie in corso');
