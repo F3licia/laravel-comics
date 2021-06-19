419ef0d4ba8ebaecf844e23474357aa3e53a3c1a
F3licia <romeofelicia@gmail.com>
2021-06-19 16:34:20 +0200

    .

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+@yield('content')
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('page_title')</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"> <!--asset = public-->
+</head>
+<body>
+
+    
+</body>
+</html>
+
