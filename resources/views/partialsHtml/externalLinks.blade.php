7a4d3a1c67d8cb6f2c988eed851ebe10ee2536a5
F3licia <romeofelicia@gmail.com>
2021-06-20 17:05:09 +0200

    sezione link

--- resources/views/partialsHtml/externalLinks.blade.php
+++ resources/views/partialsHtml/externalLinks.blade.php
@@ -1,3 +1,23 @@
-<div class="externalLinks">
-    
+<div class="section externalLinks">
+    <div class="linkSect">
+        <img src="/images/buy-comics-digital-comics.png" alt="">
+        <h4>digital comics</h4>
+    </div>
+    <div class="linkSect">
+        <img src="/images/buy-comics-merchandise.png" alt="">
+        <h4>dc merchandise</h4>
+    </div>
+    <div class="linkSect">
+        <img src="/images/buy-comics-subscriptions.png" alt="">
+        <h4>subscription</h4>
+    </div>
+    <div class="linkSect">
+        <img src="/images/buy-comics-shop-locator.png" alt="">
+        <h4>comic shop locator</h4>
+    </div>
+    <div class="linkSect">
+        <img src="/images/buy-dc-power-visa.png" alt="">
+        <h4>dc power visa</h4>
+    </div>
+
 </div>
