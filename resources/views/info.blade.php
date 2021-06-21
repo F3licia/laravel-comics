c0c55ce4d6e56037d1e4475fec172c207ca21b7a
F3licia <romeofelicia@gmail.com>
2021-06-21 19:50:37 +0200

    stampate specifiche

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -5,7 +5,6 @@
 
 
 <div class="section singleComicCont">
-  
     <div class="divider">
         <div class="coverCont">
             <img src="{{$comic['thumb']}}" alt="">       
@@ -23,9 +22,65 @@
         </div>
         
         <p>{{ $comic['description']}}</p>
-    </div>
-       
+    </div>  
 </div>
+<div class="specs">
+ 
+    <div class="specsCol">
+       <h2>Talent</h2>
+       <div class="specsSect">
+           <div class="specsTag">
+              Art by:
+           </div>
+           <div>
+                @foreach($comic['artists'] as  $artist)
+                <h4>{{ $artist}}</h4>
+                @endforeach
+           </div>
+       </div>
+       <div class="specsSect">
+        <div class="specsTag">
+           Written by:
+        </div>
+        <div>
+             @foreach($comic['writers'] as  $writer)
+             <h4>{{ $writer}}</h4>
+             @endforeach
+        </div>
+    </div>
+    </div>
+
+    <div class="specsCol">
+        <h2>Specs</h2>
+        <div class="specsSect">
+            <div class="specsTag">
+                Series:
+            </div>
+            <div>      
+                {{ $comic['series']}}  
+            </div>
+        </div>
+        <div class="specsSect">
+            <div class="specsTag">
+                Price:
+            </div>
+            <div>      
+                {{ $comic['price']}}  
+            </div>
+        </div>
+        <div class="specsSect">
+            <div class="specsTag">
+                On Sale Date:
+            </div>
+            <div>      
+                {{ $comic['sale_date']}}  
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 
 
 
